0771842ba9168711eac3a924c927b2a4b561ffa5
DibsSingh <dibs_singh@hotmail.com>
2019-11-07 15:24:37 +0000

    intitla commit

---
+++ Sender/Controller/ContactFormSurfaceController.cs
@@ -0,0 +1,15 @@
+﻿using Sender.Models;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace Sender.Controller
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactFormViewModel());
+        }
+    }
+}
---
+++ Sender/Models/ContactFormViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Sender.Models
+{
+    public class ContactFormViewModel
+    {
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string Message { get; set; }
+    }
+}
